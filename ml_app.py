96c30d3b7e472d8aae81f6fa8d41b63b8ad692ba
eparamasari <eparamasari@gmail.com>
2023-12-22 21:02:20 +0700

    first commit

---
+++ ml_app.py
@@ -0,0 +1,17 @@
+import re
+import gradio as gr
+
+def data_processing(text):
+    return re.sub(r'[^a-zA-Z0-9]', ' ', text)
+
+gradio_ui = gr.Interface(
+    fn = data_processing,
+    title = "Data Processing and Modeling",
+    description = "Aplikasi Web Data Processing dan Modeling",
+    inputs = gr.Textbox(lines=10, label="Paste some text here"),
+    outputs=[
+        gr.Textbox(label="Result")
+    ],
+)
+
+gradio_ui.launch()
